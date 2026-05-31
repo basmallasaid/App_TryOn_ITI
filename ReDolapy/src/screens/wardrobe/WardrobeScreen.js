255f4a251246c0ee507c1c229eb2d8aa1f944ebf
Menna-W-Fadel <mennafadel12@gmail.com>
2026-05-31 23:03:53 +0300

    folders structure

---
+++ ReDolapy/src/screens/wardrobe/WardrobeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const WardrobeScreen = () => {
+    return (
+        <View>
+            
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default WardrobeScreen;

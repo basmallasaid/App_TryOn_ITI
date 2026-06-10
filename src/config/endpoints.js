d64a76314e0d04312e07fbc41d7fa8cbb9313a20
Menna-W-Fadel <mennafadel12@gmail.com>
2026-06-10 21:00:41 +0300

    feat: add SelectionModal component; update wardrobe screen to use modal for image source selection; refactor API endpoints and improve wardrobe categories

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -12,5 +12,8 @@ export const ENDPOINTS = {
   SELECT_LANG:"/users/settings/language",
   GET_USER_PROFILE:"/users",
   GET_ALL_PRODUCT:"/products",
-  GET_ALL_STORE:"/stores"
+  GET_ALL_STORE:"/stores",
+  ANALYZE:"/analyze",
+  SAVETOWARDROBE:'/wardrobe/from-analysis',
+  WARDROBE:"/wardrobe"
 };
